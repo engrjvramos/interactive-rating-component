29124633db7ac47046f9c451567811c426c15c13
Jose Ramos <engrjvramos@gmail.com>
2022-03-28 16:08:11 +0800

    app is working smoothly

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 'use strict';
 let ratings = document.querySelectorAll('.rating');
+let userSelection;
+let isSelected = false;
 
 ratings.forEach((rating) => {
   rating.addEventListener('click', () => {
     ratings.forEach((el) => el.classList.remove('active'));
-
     rating.classList.add('active');
+    userSelection = rating.innerHTML;
+    isSelected = true;
   });
 });
-
-let y = 0;
 
 const submitBtn = document.getElementById('submit');
 const thankyouCard = document.querySelector('.thankyou-card');
 const ratingCard = document.querySelector('.rating-card');
+const userRating = document.getElementById('userRating');
 
 submitBtn.addEventListener('click', () => {
-  thankyouCard.setAttribute('data-visible', true);
-  ratingCard.setAttribute('data-visible', false);
+  if (isSelected) {
+    thankyouCard.setAttribute('data-visible', true);
+    ratingCard.setAttribute('data-visible', false);
+    userRating.innerHTML = userSelection;
+  } else {
+    alert('Please select a rating!');
+  }
 });
