02a79830fd83c7d856461d4dc456267b6cb3541d
Jose Ramos <engrjvramos@gmail.com>
2022-03-28 15:41:12 +0800

    finished thankyou card and submit functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+let ratings = document.querySelectorAll('.rating');
+
+ratings.forEach((rating) => {
+  rating.addEventListener('click', () => {
+    ratings.forEach((el) => el.classList.remove('active'));
+
+    rating.classList.add('active');
+  });
+});
+
+let y = 0;
+
+const submitBtn = document.getElementById('submit');
+const thankyouCard = document.querySelector('.thankyou-card');
+const ratingCard = document.querySelector('.rating-card');
+
+submitBtn.addEventListener('click', () => {
+  thankyouCard.setAttribute('data-visible', true);
+  ratingCard.setAttribute('data-visible', false);
+});
